c285d3e4c951ab95da54d652cd0449bd44c60381
Zanark <kiodeba11@gmail.com>
2017-12-23 18:11:46 +0530

    Solved Q6

---
+++ Python/Q6/q6.py
@@ -0,0 +1,34 @@
+'''
+
+# Problem 6 
+
+### Sum square difference
+
+
+The sum of the squares of the first ten natural numbers is,
+
+1**2 + 2**2 + ... + 10**2 = 385
+
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)**2 = 552 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers 
+and the square of the sum is 3025 − 385 = 2640.
+
+**Find the difference between the sum of the squares of the first one hundred natural numbers 
+and the square of the sum.**
+
+'''
+#PROGRAM:
+
+s1 = 0
+s2 = 0
+
+for i in range(1,101):
+    s1 += i**2
+    s2 += i
+
+s2 = s2**2
+
+print(s2-s1)
